11bf85dcbeb90ad65c91c23148272b9b4e7e0612
Dominik Zaczek <dominik.brendan@gmail.com>
2021-03-03 22:59:34 +0000

    Final deployment for 3/03/2021

--- pages/api/contentHandle.js
+++ pages/api/contentHandle.js
@@ -1,11 +1,11 @@
 import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
 
 const client = new ApolloClient({
-    uri: 'http://rare.dominikbrendan.com/graphql',
+    uri: 'https://rare.dominikbrendan.com/graphql',
     cache: new InMemoryCache()
   });
 export default async(req, res) => {
-    const id = req.body
+    const id = req.body.id
     try{
         const {data} = await client.query({
             query: gql`
